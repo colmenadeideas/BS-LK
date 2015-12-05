d0799f24183344336a453ce5166ec864c01dd46b
e0979 <dlarez@besign.com.ve>
2015-12-05 07:59:59 -0430

    up to date 041215

--- app/views/app/board/add.php
+++ app/views/app/board/add.php
@@ -1,8 +1,11 @@
 
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center centered-div">
 	<form id="" action="" method="POST">
-		agregar nombre
-		redes
+		<div class="board card col-lg-4 col-md-4">
+			<h2>Crear tablero</h2>
+			<input type="text" name="text" class="form-control input-lg" placeholder="Nombre para el Tablero">
+			<button type="submit" class="btn btn-default btn-lg send comment-submit">Enviar</button>
+		</div>
 	</form>
 	
 </div>
